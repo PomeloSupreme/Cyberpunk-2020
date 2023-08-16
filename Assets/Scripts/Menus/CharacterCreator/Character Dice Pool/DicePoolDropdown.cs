b119239b5b9fc8860921a08218dbade1329a6879
PomeloSupreme <108231806+PomeloSupreme@users.noreply.github.com>
2023-08-15 22:36:25 -0400

    Start Screen Fixed
    Start Screen Now can go forward and quit

--- Assets/Scripts/Menus/CharacterCreator/Character Dice Pool/DicePoolDropdown.cs
+++ Assets/Scripts/Menus/CharacterCreator/Character Dice Pool/DicePoolDropdown.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DicePoolDropdown : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    TMP_Dropdown IntelligenceDropdown;
 
-    // Update is called once per frame
-    void Update()
+    public void OnDropdownValueChange()
     {
+        List<string> OriginalList = new List<string>();
+        IntelligenceDropdown.GetComponent<TMP_Dropdown.Opt>();
+
         
     }
 }
