45a10a8e140c7b91850f9978e066b6217c2e0721
PomeloSupreme <108231806+PomeloSupreme@users.noreply.github.com>
2023-08-18 21:13:08 -0400

    Dice Roll Still Fucked Up
    Think ive figured out the problem in the logic of the lists appending and removing

--- Assets/Scripts/Menus/CharacterCreator/Character Dice Pool/DicePoolDropdown.cs
+++ Assets/Scripts/Menus/CharacterCreator/Character Dice Pool/DicePoolDropdown.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.NetworkInformation;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
@@ -16,7 +19,7 @@ public class DicePoolDropdown : MonoBehaviour
     
 
 
-
+    //8.18.23 The logic is kind of fucked. Need to adjust lists to subtract at index selected value, and then append the currentSelection to the front of the list. 
     public void Start()
     {
         dicePoolDropdowns = GameObject.FindGameObjectsWithTag("DicePoolDropdown");
@@ -27,30 +30,80 @@ public class DicePoolDropdown : MonoBehaviour
     public void OnDropdownValueChange()
     {
         optionDependentDiceRolls = dicePoolButton.ReportOptionDependentDiceRolls();
-
+        List<string> appendingString = dicePoolButton.ReportOptionDependentDiceRolls();
         int selectedValue = GetComponent<TMP_Dropdown>().value;
         string currentSelection = GetComponent<TMP_Dropdown>().options[selectedValue].text;
+
         if(currentSelection == "--")
         {
-            optionDependentDiceRolls.Add(previousSelection);
-            foreach(GameObject dicePoolDropdown in dicePoolDropdowns)
+            if(previousSelection == "--")
             {
-                dicePoolDropdown.GetComponent<TMP_Dropdown>().ClearOptions();
-                dicePoolDropdown.GetComponent<TMP_Dropdown>().AddOptions(optionDependentDiceRolls);
+                previousSelection = currentSelection;
+                foreach (GameObject dicePoolDropdown in dicePoolDropdowns)
+                {
+                    if (dicePoolDropdown.GetComponent<TMP_Dropdown>().options[0].text == "--")
+                    {
+                        dicePoolDropdown.GetComponentInChildren<TMP_Dropdown>().ClearOptions();
+                        dicePoolDropdown.GetComponentInChildren<TMP_Dropdown>().AddOptions(optionDependentDiceRolls);
+                    }
+                }
+            }
+            else
+            optionDependentDiceRolls.RemoveAt(selectedValue);
+            optionDependentDiceRolls.Insert(1, previousSelection);
+            previousSelection = currentSelection;
+
+            foreach (GameObject dicePoolDropdown in dicePoolDropdowns)
+            {
+                if (dicePoolDropdown.GetComponent<TMP_Dropdown>().options[0].text == "--")
+                {
+                    dicePoolDropdown.GetComponentInChildren<TMP_Dropdown>().ClearOptions();
+                    dicePoolDropdown.GetComponentInChildren<TMP_Dropdown>().AddOptions(optionDependentDiceRolls);
+                }
             }
         }
+        else if( currentSelection != previousSelection)
+        {
+            if (previousSelection == "--")
+            {
+                previousSelection = currentSelection;
+                optionDependentDiceRolls.RemoveAt(selectedValue);
+                foreach (GameObject dicePoolDropdown in dicePoolDropdowns)
+                {
+                    if (dicePoolDropdown.GetComponent<TMP_Dropdown>().options[0].text == "--")
+                    {
+                        dicePoolDropdown.GetComponentInChildren<TMP_Dropdown>().ClearOptions();
+                        dicePoolDropdown.GetComponentInChildren<TMP_Dropdown>().AddOptions(optionDependentDiceRolls);
+                    }
+                }
+            }
+            else
+            optionDependentDiceRolls.Remove(currentSelection);
+            optionDependentDiceRolls.Insert(1, previousSelection);
+            previousSelection = currentSelection;
+
+            foreach (GameObject dicePoolDropdown in dicePoolDropdowns)
+            {
+                if(dicePoolDropdown.GetComponent<TMP_Dropdown>().options[0].text == "--")
+                    {
+                    dicePoolDropdown.GetComponentInChildren<TMP_Dropdown>().ClearOptions();
+                    dicePoolDropdown.GetComponentInChildren<TMP_Dropdown>().AddOptions(optionDependentDiceRolls);
+                }
+            }
+        }
+
         else
         {
-            optionDependentDiceRolls.Remove(currentSelection);
-            optionDependentDiceRolls.Add(previousSelection);
-            previousSelection= currentSelection;
-
-            foreach(GameObject dicePoolDropdown in dicePoolDropdowns)
+            foreach (GameObject dicePoolDropdown in dicePoolDropdowns)
             {
                 dicePoolDropdown.GetComponentInChildren<TMP_Dropdown>().ClearOptions();
                 dicePoolDropdown.GetComponentInChildren<TMP_Dropdown>().AddOptions(optionDependentDiceRolls);
             }
         }
+        
+        appendingString.Insert(0, currentSelection);
+        GetComponent<TMP_Dropdown>().ClearOptions();
+        GetComponent<TMP_Dropdown>().AddOptions(appendingString);
         dicePoolButton.UpdateOptionDependentDiceRolls(optionDependentDiceRolls);        
     }
 }
