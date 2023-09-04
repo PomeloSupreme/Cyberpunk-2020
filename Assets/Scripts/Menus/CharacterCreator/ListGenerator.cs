6abf2ac53a65e2be8df5e3abf5374cce22e351eb
PomeloSupreme <108231806+PomeloSupreme@users.noreply.github.com>
2023-09-04 18:03:20 -0400

    AUtomating Panels

--- Assets/Scripts/Menus/CharacterCreator/ListGenerator.cs
+++ Assets/Scripts/Menus/CharacterCreator/ListGenerator.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ListGenerator : MonoBehaviour
 {
+    public string ParentName;
     public GameObject ObjectToBeReplicated;
     public List<GameObject> listOfObjects;
     public int NumberOfObjectsToMake;
@@ -18,6 +21,7 @@ public class ListGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GetComponent<TMP_Text>().text = ParentName;
         parentPosition = transform.position;
         yValueOfObject = ObjectToBeReplicated.GetComponent<RectTransform>().rect.height;
         //positionChange = new Vector3(0, yValueOfObject, 0);
@@ -40,5 +44,9 @@ public class ListGenerator : MonoBehaviour
         }
     }
 
+    public float HeightOfListGenerator()
+    {
+        return this.GetComponent<RectTransform>().rect.height;
+    }
 
 }
---
+++ Assets/Scripts/Menus/CharacterCreator/SkillPanelFiller.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillPanelFiller : MonoBehaviour
+{
+    public string PanelName;
+    public List<GameObject> ListGenerators= new List<GameObject>();
+    float totalYMoved = 0;
+    Vector3 panelPosition;
+    float panelHeight;
+    float timesToShiftRight = 1;
+    int objectsToMoveRight;
+    float listGeneratorWidth;
+
+    private void Start()
+    {
+        panelPosition = transform.position;
+        panelHeight = this.GetComponent<RectTransform>().rect.height;
+        for(int i = 0; i < ListGenerators.Count; i++)
+        {
+            float yMovement = 0;
+            ListGenerator currentListGenerator = ListGenerators[i].GetComponent<ListGenerator>();
+            listGeneratorWidth = currentListGenerator.GetComponent<RectTransform>().rect.width;
+            Vector3 rightShift = new Vector3(timesToShiftRight, 1, 1);
+            Vector3 currentVector = currentListGenerator.transform.position ;
+            currentListGenerator.transform.position = Vector3.Scale(rightShift, currentVector) + new Vector3(0, -totalYMoved, 0);
+            yMovement += ((currentListGenerator.ObjectToBeReplicated.GetComponent<RectTransform>().rect.height * currentListGenerator.SkillNames.Count) + currentListGenerator.HeightOfListGenerator()); 
+            totalYMoved+= yMovement;
+            if (totalYMoved > panelHeight)
+            {
+                timesToShiftRight++;
+                objectsToMoveRight = (int)Math.Ceiling((totalYMoved - panelHeight)/currentListGenerator.ObjectToBeReplicated.GetComponent<RectTransform>().rect.height);
+                int listOfObjectSize = currentListGenerator.listOfObjects.Count;
+                totalYMoved = 0;
+                while (objectsToMoveRight> 0)
+                {
+                    currentListGenerator.listOfObjects[listOfObjectSize - objectsToMoveRight].GetComponent<RectTransform>().position = Vector3.Scale(rightShift, currentVector);
+                    totalYMoved += currentListGenerator.ObjectToBeReplicated.GetComponent<RectTransform>().rect.height;
+                }
+            }
+
+        }
+    }
+
+}
