0eb7c888d181473befd753d04454fee316025445
PomeloSupreme <108231806+PomeloSupreme@users.noreply.github.com>
2023-09-08 21:16:44 -0400

    Infinite Loop

--- Assets/Scripts/Menus/CharacterCreator/ListGenerator.cs
+++ Assets/Scripts/Menus/CharacterCreator/ListGenerator.cs
@@ -17,19 +17,22 @@ public class ListGenerator : MonoBehaviour
     float yValueOfObject;
     float xValueOfObject;
     float positionFromListHead = 0;
+    public float spacingBetweenSkills;
+    public float distanceFromParent;
     Vector3 parentPosition;
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<TMP_Text>().text = ParentName;
-        parentPosition = transform.position;
+        parentPosition = transform.position + new Vector3 (0, distanceFromParent);
         yValueOfObject = ObjectToBeReplicated.GetComponent<RectTransform>().rect.height;
-        //positionChange = new Vector3(0, yValueOfObject, 0);
+        //Vector3 positionChange = new Vector3(0, yValueOfObject, 0);
 
         for (int i = 0; i< SkillNames.Count; i++)
         {
-            parentPosition -= new Vector3(0, yValueOfObject);
+            parentPosition -= new Vector3(0, yValueOfObject + spacingBetweenSkills);
             listOfObjects.Add(Instantiate(ObjectToBeReplicated, parentPosition , Quaternion.identity, this.transform));
+           // listOfObjects.Add(Instantiate(ObjectToBeReplicated, this.transform));
             
             
         }
--- Assets/Scripts/Menus/CharacterCreator/SkillPanelFiller.cs
+++ Assets/Scripts/Menus/CharacterCreator/SkillPanelFiller.cs
@@ -7,48 +7,106 @@ public class SkillPanelFiller : MonoBehaviour
 {
     public string PanelName;
     public List<GameObject> ListGenerators= new List<GameObject>();
-    Vector3 topRightOfPanel;
+    Vector3 topLeftOfPanel;
     float totalYMoved = 0;
     Vector3 panelPosition;
     float panelHeight;
     float timesToShiftRight = 0;
     int objectsToMoveRight;
     float listGeneratorWidth;
-    float panelYPosition;
+    public float xAdjuster;
+    public float segmentGap;
+    public float columnGap;
 
     private void Start()
     {
         panelPosition = transform.position;
-        topRightOfPanel = new Vector3 (-((GetComponent<RectTransform>().rect.width)/2),(GetComponent<RectTransform>().rect.height/2), 0);
-        panelYPosition = transform.position.y;
+        topLeftOfPanel = panelPosition + new Vector3(-GetComponent<RectTransform>().rect.width / 2, (GetComponent<RectTransform>().rect.height / 2) -15, 0);
         panelHeight = this.GetComponent<RectTransform>().rect.height;
         for (int i = 0; i < ListGenerators.Count; i++)
         {
+            
             ListGenerator currentListGenerator = ListGenerators[i].GetComponent<ListGenerator>();
             listGeneratorWidth = currentListGenerator.GetComponent<RectTransform>().rect.width;
-            Vector3 rightShift = new Vector3(timesToShiftRight, 1, 1);
-            //Vector3 currentVector = currentListGenerator.transform.position ;
-            float distanceToMoveRight = currentListGenerator.GetComponent<RectTransform>().rect.width * timesToShiftRight;
-            currentListGenerator.transform.position = topRightOfPanel + new Vector3(0, -totalYMoved, 0) + new Vector3(distanceToMoveRight, 0, 0) + new Vector3 (listGeneratorWidth/2,0,0);
-            totalYMoved += ((currentListGenerator.ObjectToBeReplicated.GetComponent<RectTransform>().rect.height * currentListGenerator.SkillNames.Count) + currentListGenerator.HeightOfListGenerator()); 
-            if (totalYMoved > panelHeight)
+            if (panelHeight - totalYMoved < 2 * currentListGenerator.GetComponent<RectTransform>().rect.height)
             {
                 timesToShiftRight++;
-                objectsToMoveRight = (int)Math.Ceiling((totalYMoved - panelHeight)/currentListGenerator.ObjectToBeReplicated.GetComponent<RectTransform>().rect.height);
-                int listOfObjectSize = currentListGenerator.listOfObjects.Count;
+                float distanceToMoveRight = (currentListGenerator.GetComponent<RectTransform>().rect.width * timesToShiftRight) - xAdjuster;
                 totalYMoved = 0;
-                float parentXPosition = currentListGenerator.transform.position.x;
-                distanceToMoveRight = currentListGenerator.GetComponent<RectTransform>().rect.width * timesToShiftRight;
-
-                while (objectsToMoveRight> 0)
+                currentListGenerator.transform.position = topLeftOfPanel + new Vector3(distanceToMoveRight, -totalYMoved, 0) + new Vector3(listGeneratorWidth / 2, 0, 0) + new Vector3 (xAdjuster, segmentGap, 0);
+                totalYMoved += ((currentListGenerator.ObjectToBeReplicated.GetComponent<RectTransform>().rect.height * currentListGenerator.SkillNames.Count) + currentListGenerator.HeightOfListGenerator());
+                if (totalYMoved > panelHeight)
                 {
-                    currentListGenerator.listOfObjects[listOfObjectSize - objectsToMoveRight].GetComponent<RectTransform>().position =  topRightOfPanel + new Vector3(distanceToMoveRight, -totalYMoved, 0);
-                    totalYMoved += currentListGenerator.ObjectToBeReplicated.GetComponent<RectTransform>().rect.height;
-                    objectsToMoveRight--;
+                    timesToShiftRight++;
+                    objectsToMoveRight = (int)Math.Ceiling((totalYMoved - panelHeight) / currentListGenerator.ObjectToBeReplicated.GetComponent<RectTransform>().rect.height);
+                    int listOfObjectSize = currentListGenerator.listOfObjects.Count;
+                    totalYMoved = 0;
+                    distanceToMoveRight = (currentListGenerator.GetComponent<RectTransform>().rect.width * timesToShiftRight) - xAdjuster;
+
+                    while (objectsToMoveRight > 0)
+                    {
+                        currentListGenerator.listOfObjects[listOfObjectSize - objectsToMoveRight].GetComponent<RectTransform>().position = topLeftOfPanel + new Vector3(distanceToMoveRight, -totalYMoved, 0) + new Vector3(listGeneratorWidth / 2, 0, 0);
+                        totalYMoved += currentListGenerator.ObjectToBeReplicated.GetComponent<RectTransform>().rect.height;
+                        objectsToMoveRight--;
+                    }
                 }
             }
+            else
+            {
+                //Vector3 currentVector = currentListGenerator.transform.position ;
+                float distanceToMoveRight = (currentListGenerator.GetComponent<RectTransform>().rect.width * timesToShiftRight) - xAdjuster;
+                currentListGenerator.transform.position = topLeftOfPanel + new Vector3(distanceToMoveRight, -totalYMoved, 0) + new Vector3(listGeneratorWidth / 2, 0, 0) + new Vector3(xAdjuster, segmentGap, 0);
+                totalYMoved += ((currentListGenerator.ObjectToBeReplicated.GetComponent<RectTransform>().rect.height * currentListGenerator.SkillNames.Count) + currentListGenerator.HeightOfListGenerator());
+                if (totalYMoved > panelHeight)
+                {
+                    timesToShiftRight++;
+                    objectsToMoveRight = (int)Math.Ceiling((totalYMoved - panelHeight) / currentListGenerator.ObjectToBeReplicated.GetComponent<RectTransform>().rect.height);
+                    int listOfObjectSize = currentListGenerator.listOfObjects.Count;
+                    totalYMoved = 0;
+                    distanceToMoveRight = (currentListGenerator.GetComponent<RectTransform>().rect.width * timesToShiftRight) - xAdjuster;
 
+                    while (objectsToMoveRight > 0)
+                    {
+                        currentListGenerator.listOfObjects[listOfObjectSize - objectsToMoveRight].GetComponent<RectTransform>().position = topLeftOfPanel + new Vector3(distanceToMoveRight, -totalYMoved, 0) + new Vector3(listGeneratorWidth / 2, 0, 0);
+                        totalYMoved += currentListGenerator.ObjectToBeReplicated.GetComponent<RectTransform>().rect.height;
+                        objectsToMoveRight--;
+                    }
+                }
+            }
         }
-    }   
+        float scale = 1;
+        listGeneratorWidth = 600;
+        //float listGeneratorWidth = this.GetComponent<RectTransform>().rect.width;
+        //if (timesToShiftRight * listGeneratorWidth > this.GetComponent<RectTransform>().rect.width)
+        if (timesToShiftRight * listGeneratorWidth > listGeneratorWidth)
+        {
+            //while (timesToShiftRight * listGeneratorWidth > this.GetComponent<RectTransform>().rect.width)
+            while (timesToShiftRight * listGeneratorWidth > listGeneratorWidth)
+            {
+                foreach (Transform transform in transform)
+                {
+                    transform.localScale = (new Vector3(scale, scale, scale));
+                    listGeneratorWidth *= scale;
+                    scale -= 0.1f;
+                }
+            }
+        }
+    }
+    private void Update()
+    {
+        float scale = 1;
+
+        if(timesToShiftRight*listGeneratorWidth > this.GetComponent<RectTransform>().rect.width)
+        {
+            while(timesToShiftRight * listGeneratorWidth > this.GetComponent<RectTransform>().rect.width)
+            {
+                foreach (Transform transform in transform)
+                {
+                    transform.localScale = (new Vector3(scale, scale, scale));
+                    scale--;
+                }
+            }
+        }
+    }
 
 }
