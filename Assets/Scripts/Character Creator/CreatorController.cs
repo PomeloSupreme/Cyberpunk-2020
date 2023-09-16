de48d93c550aa9592e862c33f8eb23c0378a3040
PomeloSupreme <108231806+PomeloSupreme@users.noreply.github.com>
2023-09-16 15:02:45 -0400

    Stats

--- Assets/Scripts/Character Creator/CreatorController.cs
+++ Assets/Scripts/Character Creator/CreatorController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CreatorController : MonoBehaviour
@@ -7,13 +8,106 @@ public class CreatorController : MonoBehaviour
     public GameObject PanelParent;
     private List<GameObject> panels = new List<GameObject>();
 
+    List<int> statValues = new List<int>();
+
+    enum Stat
+    {
+        Intelligence,
+        Reflex,
+        Technical,
+        Cool,
+        Attractiveness,
+        Luck,
+        Movement,
+        Body,
+        Empathy,
+    }
+
+
+
     private void Start()
     {
+        for(int i = 0; i < statValues.Count; i++)
+        {
+            statValues[i] = 2;
+        }
         foreach (GameObject panel in panels)
         {
             panels.Add(panel);
         }
+
+        Debug.Log((int)Stat.Intelligence);
     }
 
-
+    private int determineStatPositionInList (string stat)
+    {
+        int statListPosition;
+        switch (name)
+        {
+            case "Intelligence":
+                statListPosition = 0;
+                break;
+            case "Reflex":
+                statListPosition = 1;
+                break;
+            case "Technical":
+                statListPosition = 2;
+                break;
+            case "Cool":
+                statListPosition = 3;
+                break;
+            case "Attractiveness":
+                statListPosition = 4;
+                break;
+            case "Luck":
+                statListPosition = 5;
+                break;
+            case "Movement":
+                statListPosition = 6;
+                break;
+            case "Body":
+                statListPosition = 7;
+                break;
+            case "Empathy":
+                statListPosition = 8;
+                break;
+            default:
+                Debug.Log("Error In DetermineStatPositionInList Function in Creator Controller Script. String Not Found");
+                statListPosition = 10;
+                break;
+        }
+        return statListPosition;
+    }
+    public int ReturnStatValue(string name)
+    {
+        int statPositionInList = determineStatPositionInList(name);
+        return statValues[statPositionInList];
+    }
+    public int OnStatButtonReturnsStatValue(string name, bool isPLus)
+    {
+        int statPositionInList = determineStatPositionInList(name);
+        int currentStatValue = statValues[statPositionInList];
+        if (isPLus)
+        {
+            if (currentStatValue >= 0
+                && currentStatValue < 10)
+            {
+                statValues[statPositionInList]++;
+            }
+        }
+        else if (!isPLus)
+        {
+            if (currentStatValue <= 10
+                && currentStatValue > 0)
+            {
+                statValues[statPositionInList]--;
+            }
+        }
+        else
+        {
+            Debug.Log("Error in OnStatButton, isPlus was not read properly");
+        }
+        return statValues[statPositionInList];
+    }
+     
 }
--- Assets/Scripts/Character Creator/Prefab Scripts/Stat.cs
+++ Assets/Scripts/Character Creator/Prefab Scripts/Stat.cs
@@ -9,6 +9,7 @@ using UnityEngine.EventSystems;
 
 public class Stat : MonoBehaviour
 {
+    public CreatorController creatorController;
     public float HowMuchToFade;
     public string Name;
     public string Description;
@@ -20,7 +21,8 @@ public class Stat : MonoBehaviour
 
     private void Start()
     {
-        GetComponent<TMP_Text>().text= Name;
+        creatorController = GetComponentInParent<CreatorController>();
+        GetComponent<TMP_Text>().text= Name + ":";
         DescriptionText.GetComponentInChildren<TMP_Text>().text = Description;
         DescriptionText.SetActive(false);
     }
@@ -45,12 +47,32 @@ public class Stat : MonoBehaviour
 
     public void OnMinusButton()
     {
-       
+        int currentValue = creatorController.OnStatButtonReturnsStatValue(Name, false);
+        
+        if (currentValue == 2)
+        {
+            buttonMinus.GetComponent<GameObject>().SetActive(false);
+        }
+        if (currentValue == 9)
+        {
+            buttonPlus.GetComponent<GameObject>().SetActive(true);
+        }
+        SetStatTextToControllerList();
     }
 
     public void OnPlusButton()
     {
+        int currentValue = creatorController.OnStatButtonReturnsStatValue(name, true);
 
+        if (currentValue == 3)
+        {
+            buttonMinus.GetComponent<GameObject>().SetActive(true);
+        }
+        if (currentValue == 10)
+        {
+            buttonPlus.GetComponent<GameObject>().SetActive(false);
+        }
+        SetStatTextToControllerList();
     }
 
     private void ChangeTransparencyForImage(GameObject gameObject, float changeTransparency)
@@ -64,6 +86,37 @@ public class Stat : MonoBehaviour
         var color = gameObject.GetComponent<TMP_Text>().color;
         color.a= changeTransparency;
         gameObject.GetComponent<TMP_Text>().color = color;
+    }
+    private void SetStatTextToControllerList()
+    {
+        int currentValue = creatorController.ReturnStatValue(Name);
+        StatValue.GetComponent<TMP_Text>().text = ConvertIntToTextAndDetermineZero(currentValue);
+    }
+    private string ConvertIntToTextAndDetermineZero(int value)
+    {
+        if( value < -9)
+        {
+            return value.ToString();
+        }
+        else if (value < 0
+            && value >= -9)
+        {
+            return "-0" + (value * -1).ToString();
+        }
+        else if(value <= 9
+            && value > 0)
+        {
+            return "0" + value.ToString();
+        }
+        else  if(value > 9)
+        {
+            return value.ToString();
+        }
+        else
+        {
+            Debug.Log("Error in ConvertIntToText in Stat Script");
+            return "ERROR";
+        }
     }
 
 }
